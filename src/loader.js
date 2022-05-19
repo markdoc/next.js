5bedcfefd0af8222f85a26abb7d61516d2df045e
Mike Fix <mfix@stripe.com>
2022-05-19 16:35:33 -0700

    try using .posix everywhere

--- src/loader.js
+++ src/loader.js
@@ -1,16 +1,8 @@
 const fs = require('fs');
-const path = require('path');
+const {posix: path} = require('path');
 const Markdoc = require('@markdoc/markdoc');
 
 const DEFAULT_SCHEMA_PATH = './markdoc';
-
-function normalizeAbsolutePath(s) {
-  // https://stackoverflow.com/questions/53799385/how-can-i-convert-a-windows-path-to-posix-path-using-node-path
-  return s
-    .replace(/^[a-zA-Z]:/, '') // replace C: for Windows
-    .split(path.sep)
-    .join(path.posix.sep);
-}
 
 async function gatherPartials(ast, schemaDir) {
   let partials = {};
@@ -134,9 +126,7 @@ async function load(source) {
 
     async function readDir(variable) {
       try {
-        const module = normalizeAbsolutePath(
-          await resolve(schemaDir, variable)
-        );
+        const module = await resolve(schemaDir, variable);
         return `import * as ${variable} from '${module}'`;
       } catch (error) {
         return `const ${variable} = {};`;
@@ -171,9 +161,7 @@ import yaml from 'js-yaml';
 // renderers is imported separately so Markdoc isn't sent to the client
 import Markdoc, {renderers} from '@markdoc/markdoc'
 
-import {getSchema} from '${normalizeAbsolutePath(
-    await resolve(__dirname, './runtime')
-  )}';
+import {getSchema} from '${await resolve(__dirname, './runtime')}';
 /**
  * Schema is imported like this so end-user's code is compiled using build-in babel/webpack configs.
  * This enables typescript/ESnext support
