e87213a6d1b8313886629fbd8cfc29e7988ec447
Mike Fix <mfix@stripe.com>
2023-10-11 11:37:02 -0700

    update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,7 @@
+import {NextConfig} from 'next';
 import type {ElementType} from 'react';
 import type {Config, RenderableTreeNodes, Schema} from '@markdoc/markdoc';
+import type {RuleSetConditionAbsolute} from 'webpack';
 
 export type MarkdocNextJsPageProps = {
   markdoc?: {
@@ -17,3 +19,19 @@ export type MarkdocNextJsSchema<O extends Object = {}> = Schema<
   O & MarkdocNextJsConfig,
   ElementType
 >;
+
+export interface MarkdocNextJsOptions {
+  extension?: RuleSetConditionAbsolute;
+  mode?: 'static' | 'server';
+  options?: {
+    slots?: boolean;
+    allowComments?: boolean;
+  };
+  schemaPath?: string;
+}
+
+declare function createMarkdocPlugin(
+  options?: MarkdocNextJsOptions
+): (config: NextConfig) => NextConfig;
+
+export = createMarkdocPlugin;
